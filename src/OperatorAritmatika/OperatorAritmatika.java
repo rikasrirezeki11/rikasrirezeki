429cea24afe859816d7bfe149d606234c0fc0f21
rikasrirezeki11 <Rika.srirezeki@yahoo.com>
2015-10-15 09:03:31 +0700

    TUGAS2PBO

---
+++ src/OperatorAritmatika/OperatorAritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OperatorAritmatika;
+
+/**
+ *
+ * @author User
+ */
+public class OperatorAritmatika {
+ public int nilaihasil;
+ 
+ int penjumlahan (int a,int b){
+   nilaihasil=a+b;
+   return nilaihasil;
+   }
+ 
+ int pengurangan (int a,int b){
+   nilaihasil=a-b;
+   return nilaihasil;
+   }
+ int perkalian (int a,int b){
+   nilaihasil=a*b;
+   return nilaihasil;
+   }
+ double pembagian (double a,double b){
+   double hasil= a/b;
+   return nilaihasil;
+ }
+}
+   
+
